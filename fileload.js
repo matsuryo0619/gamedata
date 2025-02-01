60d715a396d825d71ef7730cb9da4973ddbb57e0
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-02-01 09:54:27 +0900

    Update fileload.js

--- fileload.js
+++ fileload.js
@@ -1,1 +1,19 @@
+const sprictTag = document.querySelector('script[src="fileload.js"]');
+const cssFiles = JSON.parse(scriptTag.dataset.cssFiles || "[]");
+const jsFiles = JSON.parse(scriptTag.dataset.jsFiles || "[]");
 
+//CSS読み込み
+cssFiles.forEach(file => {
+  const link = document.createElement('link');
+  link.rel = "stylesheet";
+  link.href = file;
+  document.head.appendChild(link);
+});
+
+//JS読み込み
+jsFiles.forEach(file => {
+  const script = document.createElement('script');
+  script.src = file.src;
+  if (file.defer) script.defer = "true";
+  document.body.appendChild(script);
+});
